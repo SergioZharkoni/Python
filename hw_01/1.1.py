2d1841578cf3b3fbb3c7fd56999128c78d7e1ee6
Sergey Zharkov <sergey.zharkov.90@mail.ru>
2023-03-18 23:45:19 +0600

    1st homework done!

---
+++ hw_01/1.1.py
@@ -0,0 +1,12 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+number = 5
+some_text = "Some, text"
+print(number)
+print(some_text)
+
+number_from_user = int(input("Введите число: "))
+text_from_user = str(input("Введите текст: "))
+print("Вы ввели число:", number_from_user)
+print("Вы ввели строку:", text_from_user)
